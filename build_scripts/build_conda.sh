25e784953febf558433513e78ec9243a44300790
Shing Chan <cshing.m@gmail.com>
2022-10-01 13:24:38 +0000

    improve build scripts; fix build_conda.sh

--- build_scripts/build_conda.sh
+++ build_scripts/build_conda.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 # Be sure to have packaged for PyPI first
-conda skeleton pypi actipy --output-dir conda-recipe
-conda build conda-recipe/actipy
+
+conda install anaconda-client &&
+conda install conda-build &&
+# TODO: allow user-specified version and append this to next line: --version x.x.x
+conda skeleton pypi stepcount --output-dir conda-recipe &&
+conda build -c conda-forge conda-recipe/stepcount
+
+printf "\nNext steps:\n-----------\n"
+printf "Login to Anaconda:\n> anaconda login\n"
+printf "\nUpload package (path is printed in previous steps):\n> anaconda upload --user oxwear /path/to/package.tar.bz2\n\n"
+
 # anaconda login
 # anaconda upload --user oxwear /path/to/package.tar.bz2
--- build_scripts/build_pypi.sh
+++ build_scripts/build_pypi.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python setup.py sdist bdist_wheel
-twine check dist/*
+python setup.py sdist bdist_wheel &&
+twine check dist/* &&
+printf "\nTo upload to Test PyPI:\n> twine upload --repository-url https://test.pypi.org/legacy/ dist/*\n" &&
+printf "\nTo upload to PyPI:\n> twine upload dist/*\n\n"
+
 # twine upload --repository-url https://test.pypi.org/legacy/ dist/*
 # twine upload dist/*
