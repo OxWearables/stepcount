b1c78dcfd3280f386b5a2512ca983af6518a2c4c
Shing Chan <cshing.m@gmail.com>
2023-02-13 15:19:02 +0000

    fix rf model (broken after 2.0.0)

--- stepcount/__init__.py
+++ stepcount/__init__.py
@@ -4,10 +4,10 @@ __author__ = "Shing Chan, Scott Small, Gert Mertes, Aiden Doherty"
 __email__ = "shing.chan@ndph.ox.ac.uk, scott.small@ndph.ox.ac.uk, gert.mertes@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
 __license__ = "2-Clause BSD"
 __model_version__ = {
-    "rf": "20221213",
+    "rf": "20230210",
     "ssl": "ssl-20230208"
 }
 __model_md5__ = {
-    "rf": "edac2d4b7d75b40f25f0becd51cf4a8f",
+    "rf": "8d803d175b792fff12f93ca37fbc9271",
     "ssl": "eea6179f079b554d5e2c8c98ccea8423"
 }
