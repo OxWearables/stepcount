2688b6941bcde4943f26f4d79d932701c1b65d43
Shing Chan <cshing.m@gmail.com>
2023-02-17 15:58:41 +0000

    v2.0.3

--- stepcount/__init__.py
+++ stepcount/__init__.py
@@ -1,5 +1,5 @@
 name = "stepcount"
-__version__ = "2.0.2"
+__version__ = "2.0.3"
 __author__ = "Shing Chan, Scott Small, Gert Mertes, Aiden Doherty"
 __email__ = "shing.chan@ndph.ox.ac.uk, scott.small@ndph.ox.ac.uk, gert.mertes@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
 __license__ = "2-Clause BSD"
