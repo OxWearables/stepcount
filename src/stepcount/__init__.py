11e9264ec9e52e78bef0b81d1e984f172cafb131
Shing Chan <cshing.m@gmail.com>
2023-04-19 19:33:07 +0100

    versioneer-enable the package

--- src/stepcount/__init__.py
+++ src/stepcount/__init__.py
@@ -1,5 +1,4 @@
 name = "stepcount"
-__version__ = "2.1.2"
 __author__ = "Shing Chan, Scott Small, Gert Mertes, Aiden Doherty"
 __email__ = "shing.chan@ndph.ox.ac.uk, scott.small@ndph.ox.ac.uk, gert.mertes@ndph.ox.ac.uk, aiden.doherty@ndph.ox.ac.uk"
 __license__ = "2-Clause BSD"
@@ -11,3 +10,6 @@ __model_md5__ = {
     "rf": "8d803d175b792fff12f93ca37fbc9271",
     "ssl": "eea6179f079b554d5e2c8c98ccea8423"
 }
+
+from . import _version
+__version__ = _version.get_versions()['version']
