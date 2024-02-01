018a841c3bfa6a330d047c0c205e1c50a132241a
Shing Chan <cshing.m@gmail.com>
2024-02-01 15:59:43 +0000

    build: bump version: hmmlearn 0.2.7 -> 0.3.*

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setuptools.setup(
         "joblib==1.2.*",
         "scikit-learn==1.1.1",
         "imbalanced-learn==0.9.1",
-        "hmmlearn==0.2.7",
+        "hmmlearn==0.3.*",
         "torch==1.13.*",
         "torchvision==0.14.*",
         "transforms3d==0.4.*"
